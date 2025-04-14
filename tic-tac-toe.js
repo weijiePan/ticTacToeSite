8d40d330d554e9ee5afccf98c5b62e9d869d6d19
weijiePan <97200483+weijiePan@users.noreply.github.com>
2025-04-14 13:56:05 -0400

    cell creation

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,47 +1,26 @@
-//setting up input table data variable
-const td1 = document.querySelector("#td1");
-const td2 = document.querySelector("#td2");
-const td3 = document.querySelector("#td3");
-const td4 = document.querySelector("#td4");
-const td5 = document.querySelector("#td5");
-const td6 = document.querySelector("#td6");
-const td7 = document.querySelector("#td7");
-const td8 = document.querySelector("#td8");
-const td9 = document.querySelector("#td9");
-//storing table value for behind-display logic
-//0 = no value, 1 = "x", 2 = "o"
-values = [[0,0,0],[0,0,0],[0,0,0]];
+let board = [];
+function createCell(){
+    let cell = document.createElement("td");
+    let icon = document.createElement("img");
+    icon.setAttribute("src", "./asset/blank128x128.png");
+    icon.addEventListener("click", function (newSource){
+        icon.setAttribute("src", newSource);//choosing how to pick where the new source is from?
+    });
+    
+    function checkIcon(){
+        return icon.getAttribute("src");
+    }
+    return{checkIcon, changeIcon};
+}
+//initates board
+for(let i = 0; i < 9; i ++){
+    board.append(createCell);
+}
+
 //setting up game trackers
 let turn = 1;
 
-//checking 
-td1.onclick = function(){
-    changeValue(0, td1);
-};
-td2.onclick = function(){
-    changeValue(1, td2);
-};
-td3.onclick = function(){
-    changeValue(2, td3);
-};
-td4.onclick = function(){
-    changeValue(3, td4);
-};
-td5.onclick = function(){
-    changeValue(4, td5);
-};
-td6.onclick = function(){
-    changeValue(5, td6);
-};
-td7.onclick = function(){
-    changeValue(6, td7);
-};
-td8.onclick = function(){
-    changeValue(7, td8);
-};
-td9.onclick = function(){
-    changeValue(8, td9);
-};
+
 
 function changeValue(order, td){
     const choiceText = td.querySelector(".choice");
