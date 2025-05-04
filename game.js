694cb7452e4ea9053c2d025e0190733aed75fb8a
weijiePan <97200483+weijiePan@users.noreply.github.com>
2025-05-03 20:18:44 -0400

    draw checking

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ function createPlayer(name, iconUrl){
    function setScore(nScore){
     _score = nScore;
    }
-   return {getIconUrl, getName, getScore, getScore};
+   return {getIconUrl, getName, setScore, getScore};
 }
 function createCell(gm){
     let cell = document.createElement("td");
@@ -26,7 +26,7 @@ function createCell(gm){
         if(icon.getAttribute("src") == "./asset/blank128x128.png"){
             gm.changeCurrPlayer(); 
             icon.setAttribute("src", gm.getCurrPlayerUrl());//choosing how to pick where the new source is from?
-            alert(gm.checkGameCondition());
+            gm.checkGameCondition();
         }
     });
     cell.appendChild(icon);
@@ -37,6 +37,7 @@ function createGameManger(player1Name, player1IconUrl , player2Name, player2Icon
     let player1 = createPlayer(player1Name,player1IconUrl);
     let player2 = createPlayer(player2Name, player2IconUrl);
     let currPlayer = player1;
+    
     let board = [];//contains 3 tr node, with 3 td node inside each tr
     let gm = {checkGameCondition, changeCurrPlayer, getCurrPlayerUrl, getBoard};
     //initiate board
@@ -53,34 +54,57 @@ function createGameManger(player1Name, player1IconUrl , player2Name, player2Icon
             row.appendChild(data.cell);
         }
     }
+    function endGame(url){
+        if(url == null){
+            alert("draw");
+            return "";
+        }
+        let winner = null;
+        if(url == player1.getIconUrl()){
+            winner = player1;
+        }else if(url == player2.getIconUrl()){
+            winner = player2;
+        }
+        winner.setScore(winner.getScore() + 1);
+        alert(`${winner.getName()} has won`);
 
+    }
     function checkGameCondition(){
         //checks win
-            //row check
+            //row check + game draw check
+            let noEmptySpace = true;//used for checking the board state for draw
+            let rowMatchFirstUrl = null;
             for(let r = 0; r < 3; r++){
                 const currTr = board[r].childNodes;
-                const firstTdUrl = currTr[0].querySelector("img").getAttribute("src");
                 let fullMatch = true;
-                for(let c = 1; c < 3 & fullMatch == true; c++){
-                    if(firstTdUrl == "./asset/blank128x128.png" || firstTdUrl != currTr[c].querySelector("img").getAttribute("src")){
+                for(let c = 0; c < 3; c++){
+                    const currTdUrl = currTr[c].querySelector("img").getAttribute("src");
+                    if(currTdUrl == "./asset/blank128x128.png" ){
                         fullMatch = false;
-
+                        noEmptySpace = false;
                     }
+                    if(c < 2){
+                        if(currTr[c + 1].querySelector("img").getAttribute("src") != currTdUrl){
+                            fullMatch = false;
+                        }
+                    }
+                    rowMatchFirstUrl = fullMatch ? currTdUrl : null;
                 }
-                if(fullMatch == true){
-                    return true;
-                }
+                
+            }
+            if(rowMatchFirstUrl != null){
+                return (endGame(rowMatchFirstUrl));
             }
             for(let c = 0; c <3; c++){
                 const firstRowUrl = board[0].childNodes[c].querySelector("img").getAttribute("src");
                 let fullMatch = true;
-                for(let r = 1; r < 3 && fullMatch == true; r++){
+                for(let r = 1; r < 3 && fullMatch == true; r++){ //the fullMatch == true ensures no unecessary iteration is needed aftering finding match
                     if(firstRowUrl == "./asset/blank128x128.png" || firstRowUrl != board[r].childNodes[c].querySelector("img").getAttribute("src")){
                         fullMatch = false;
                     }
                 }
                 if(fullMatch == true){
-                    return true;
+                    return(endGame(firstRowUrl));
                 }
                     
                 
@@ -98,8 +122,12 @@ function createGameManger(player1Name, player1IconUrl , player2Name, player2Icon
                     }
                 }
                 if(fullMatch){
-                    return fullMatch;
+                    return(endGame(prevUrl));
                 }
+                
+            }
+            if(noEmptySpace){
+                endGame();
             }
         }
                 
